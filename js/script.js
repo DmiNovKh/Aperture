19a3a33fc247849a8ef387571e949ef57a4a26bf
DmiNovKh <vtmnkh86@gmail.com>
2022-12-03 19:41:28 +0200

    resp_head

--- js/script.js
+++ js/script.js
@@ -28,60 +28,42 @@ $('.owl-carousel').owlCarousel({
 
 
 
+(function($) {
 
-
-
-
-jQuery(document).ready(function($){
-    //toggle 3d navigation
-    $('.cd-3d-nav-trigger').on('click', function(){
-        toggle3dBlock(!$('.cd-header').hasClass('nav-is-visible'));
-    });
-
-    //select a new item from the 3d navigation
-    $('.cd-3d-nav a').on('click', function(){
-        var selected = $(this);
-        selected.parent('li').addClass('cd-selected').siblings('li').removeClass('cd-selected');
-        updateSelectedNav('close');
-    });
-
-    $(window).on('resize', function(){
-        window.requestAnimationFrame(updateSelectedNav);
-    });
-
-    function toggle3dBlock(addOrRemove) {
-        if(typeof(addOrRemove)==='undefined') addOrRemove = true;
-        $('.cd-header').toggleClass('nav-is-visible', addOrRemove);
-        $('main').toggleClass('nav-is-visible', addOrRemove);
-        $('.cd-3d-nav-container').toggleClass('nav-is-visible', addOrRemove);
-    }
-
-    //this function update the .cd-marker position
-    function updateSelectedNav(type) {
-        var selectedItem = $('.cd-selected'),
-            selectedItemPosition = selectedItem.index() + 1,
-            leftPosition = selectedItem.offset().left,
-            backgroundColor = selectedItem.data('color');
-
-        $('.cd-marker').removeClassPrefix('color').addClass('color-'+ selectedItemPosition).css({
-            'left': leftPosition,
+    $(document).ready(function(){
+        $('.toggle-nav').on('click', function() {
+            toggleNavigation($(this), $('.nav-pane'));
+            changeLetters($(this));
         });
-        if( type == 'close') {
-            $('.cd-marker').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-                toggle3dBlock(false);
-            });
+
+        function toggleNavigation(btn, nav) {
+            btn.toggleClass('open');
+            nav.toggleClass('open');
         }
-    }
 
-    $.fn.removeClassPrefix = function(prefix) {
-        this.each(function(i, el) {
-            var classes = el.className.split(" ").filter(function(c) {
-                return c.lastIndexOf(prefix, 0) !== 0;
-            });
-            el.className = $.trim(classes.join(" "));
-        });
-        return this;
-    };
-});
+        function changeLetters(btn) {
+            var m = $('.toggle-nav span.m');
+            var e = $('.toggle-nav span.e');
+            var n = $('.toggle-nav span.n');
+            var u = $('.toggle-nav span.u');
+
+            e.toggleClass('btn-close');
+
+            if(btn.hasClass('open'))
+            {
+                m.text("E");
+                n.text("I");
+                u.text("T");
+            }
+            else
+            {
+                m.text("M");
+                n.text("N");
+                u.text("U");
+            }
+        }
+    });
+
+})(jQuery);
 
 
